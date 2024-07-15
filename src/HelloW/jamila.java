64aef170d22dd9227def38efb73d454d7b5782e3
Zikriya Qasimi <khatiramoradi150@gmil.com>
2024-07-15 17:03:35 -0400

    jamila jan

---
+++ src/HelloW/jamila.java
@@ -0,0 +1,9 @@
+package HelloW;
+
+import javax.annotation.processing.SupportedSourceVersion;
+
+public class jamila {
+    public static void main(String[] args) {
+        System.out.println("jamila is going some where");
+    }
+}
